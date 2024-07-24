365236deaf31a2af4637822c4b3c0eaf90782cea
Vitalian968 <git@github.com>
2024-07-24 17:38:57 +0300

    mj

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,6 +10,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['158.160.90.21', '127.0.0.1', 'localhost', 'vitaliansait.ddns.net']
 
+CSRF_TRUSTED_ORIGINS = ['https://vitaliansait.ddns.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
